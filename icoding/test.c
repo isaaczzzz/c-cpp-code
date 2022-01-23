88520ff76e331e206951f3ee975a821936c67c59
isaaczzzz <isaac-z@outlook.com>
2022-01-23 22:32:08 +0800

    update

--- icoding/test.c
+++ icoding/test.c
@@ -1,92 +1,19 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
 
-#define STACK_SIZE 100
-
-char contents[STACK_SIZE];
-int top = 0;
-
-/*
-void stack_overflow(void)
-{
-  printf("Stack overflow\n");
-  exit(EXIT_FAILURE);
-}
-
-void stack_underflow(void)
-{
-  printf("Stack underflow\n");
-  exit(EXIT_FAILURE);
-}
-*/
-
-void make_empty(void)
-{
-  top = 0;
-}
-
-bool is_empty(void)
-{
-  return top == 0;
-}
-
-bool is_full(void)
-{
-  return top == STACK_SIZE;
-}
-
-void push(char ch)
-{
-  /*if (is_full())
-    stack_overflow();
-  else*/
-    contents[top++] = ch;
-}
-
-char pop(void)
-{
-  /*if (is_empty())
-    stack_underflow();
-  else*/
-    return contents[--top];
-
-  return '\0'; /* prevents compiler warning due to stack_underflow() call */
-}
-
+unsigned char getChksum(unsigned char *p, int len);
 
 int main(void)
 {
-    char ch;
-    while((ch = getchar()) != '\n')
-    {
-        if(ch == '(' || ch == '{')
-            push(ch);
-        else if(ch == ')' || ch == '}')
-		{
-            switch (pop())
-            {
-            case '{':
-                if(ch != '}')
-				{
-                    printf("Parentheses/braces are NOT nested properly\n");
-                		return 1;
-				}
-				break;
-			case '(':
-                if(ch != ')')
-                {
-					printf("Parentheses/braces are NOT nested properly\n");
-                		return 1;
-				}
-				break;
-            }
-        }
-		else{
-			printf("Parentheses/braces are NOT nested properly\n");
-			return 1;
-		}
-    }
-    printf("Parentheses/braces are nested properly\n");
+	unsigned char id[13];
+	scanf("%s", id);
+	printf("做 %c 卷", getChksum(id, 13));
+	system("pause");
 	return 0;
+}
+
+unsigned char getChksum(unsigned char *p, int len)
+{
+	int num;
+	num = (int)*(p + len - 1);
+	return num % 2 ? 'A':'B';
 }
