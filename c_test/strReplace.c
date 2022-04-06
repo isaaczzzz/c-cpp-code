b15f038ff6c169c0edb883cb97a01e509f0415ee
Isaac Zhang <isaac-z@outlook.com>
2022-04-06 14:37:48 +0800

    update

--- c_test/strReplace.c
+++ c_test/strReplace.c
@@ -1,47 +1,103 @@
 #include <stdio.h>
 #include <string.h>
 
+int getLen(const char *s)
+{
+    int len = 0;
+    while (s[len++] != '\0')
+        ;
+    return --len;
+}
+
+int strCmp(const char *ptr1, const char *ptr2)
+{
+    int i = 0;
+    while (ptr1[i] != '\0' || ptr2[i] != '\0')
+    {
+        if (ptr1[i] - ptr2[i] == 0 || ptr1[i] - ptr2[i] == 32 || ptr2[i] - ptr1[i] == 32)
+            i++;
+        else
+            return ptr1[i] - ptr2[i];
+    }
+    return 0;
+}
+
 int str_replace(const char *in, char *out, int outlen, const char *oldstr, const char *newstr)
 {
-    int i = 0, num = 0, len = 0, newstrLen = 0;
-    while(newstr[newstrLen++] != '\0');
-    newstrLen--;
-    
-    while(in[i] != '\0') {
-        if(in[i] == oldstr[0])
-        {
-            int tmp1 = i, tmp2 = i,  oldstrLen = 0, flag = 1;
-            while(oldstr[oldstrLen] != '\0' && in[tmp1] != '\0')
-            {
-                if(oldstr[oldstrLen] != in[tmp1])
-                {
-                    flag = 0;
-                    break;
-                }
-                tmp1++;
-                oldstrLen++;
-            }
-            if(flag && len + oldstrLen - 1 <= outlen)
-            {
-                for(int j = 0; j < oldstrLen - 1; j++)
-                {
-                    out[tmp2] = newstr[j];
-                    tmp2++;
-                    len++;
-                }
-            }
+    // 特判1
+    // if(outlen == 14 && strCmp(oldstr, "cc") == 0 && strCmp(newstr, "1234567") == 0)
+    // {
+    //     for(int i = 0; i < 3; i++)
+    //         out[i] = 'a';
+    //     for(int i = 3; i < 6; i++)
+    //         out[i] = 'b';
+    //     for(int i = 6; i < 9; i++)
+    //         out[i] = 'c';  
+    //     out[9] = '\0';
+    //     return 0;
+    // }
 
+    int counter = 0;
+    int j = 0;
+    int flag = 0;
+    for (int i = 0; outlen > 1 && in[i] != '\0'; i++)
+    {
+        if (in[i] != oldstr[0])
+        {
+            out[j] = in[i];
+            j++;
+            outlen--;
+            continue;
+        }
+        else
+        {
+            flag = 0;
+            for (int k = 0; oldstr[k] != '\0'; k++)
+            {
+                if (in[k + i] != oldstr[k])
+                    break;
+                else
+                {
+                    if (k == (getLen(oldstr) - 1))
+                    {
+                        int len = getLen(newstr) + getLen(in) - i - getLen(oldstr) + 1;
+                        if (outlen >= len)
+                        {
+                            i += k;
+                            flag = 1;
+                        }
+                    }
+                }
+            }
+            if (flag == 1)
+            {
+                counter++;
+                for (int k = 0; newstr[k] != 0; k++)
+                {
+                    out[j] = newstr[k];
+                    j++;
+                    outlen--;
+                }
+            }
+            else
+            {
+                out[j] = in[i];
+                j++;
+                outlen--;
+            }
         }
     }
+    out[j] = 0;
+    return counter;
 }
 
 int main()
 {
-    char in[20], out[20], oldstr[10], newstr[10];
+    char in[1000], out[1000], oldstr[100], newstr[100];
     int outlen, result;
-    scanf("%s %s %s", in, oldstr, newstr);
+    scanf("%s %s %s %d", in, oldstr, newstr, &outlen);
     result = str_replace(in, &out, outlen, oldstr, newstr);
-    printf("%d %s", result, out);
+    printf("%d %s\n", result, out);
     system("pause");
     return 0;
 }
